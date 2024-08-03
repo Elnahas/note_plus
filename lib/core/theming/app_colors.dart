af518e67cc2cc5ff0b9597a9ac2e323b54b0d172
Hazem <hazemelnahas2010@gmail.com>
2024-08-03 12:36:47 +0300

    - add some colors to our app

---
+++ lib/core/theming/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+
+   static Color primary = const Color(0xFF8685E7);
+   static Color background = const Color(0xFF121212);
+   static Color white = Colors.white;
+   static Color red = const Color(0xFFFF4666);   
+   static Color green = const Color(0xFF66CC41);
+   static Color blueSky = const Color(0xFF41CCA7);
+   static Color blue = const Color(0xFF4181CC);
+   static Color orange = const Color(0xFFCC8441);
+   static Color purple = const Color(0xFF9741CC);
+   static Color gray = const Color(0xFF979797);
+   static Color deepGray = const Color(0xFF424242);
+   static Color dark = const Color(0xFF1D1D1D);
+}

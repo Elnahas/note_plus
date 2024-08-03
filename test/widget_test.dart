de111f223d6cc783cda18b180fcf76d48b1ccda3
Hazem <hazemelnahas2010@gmail.com>
2024-08-03 12:19:31 +0300

    - create my app

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:note_plus/main.dart';
+import 'package:note_plus/app/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {

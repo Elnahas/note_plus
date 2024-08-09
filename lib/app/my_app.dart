bd09625ad6b13ee796b641ccd00d576feb9dacfd
Hazem <hazemelnahas2010@gmail.com>
2024-08-09 19:01:03 +0300

    - add app router

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:note_plus/core/helpers/app_string.dart';
+import 'package:note_plus/core/routing/app_router.dart';
+
+import '../core/routing/routes.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final AppRouter appRouter ;
+  const MyApp({super.key, required this.appRouter});
 
   @override
   Widget build(BuildContext context) {
@@ -13,7 +17,9 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Scaffold(body: Center(child: Text('Hello World')),),
+      onGenerateRoute: appRouter.onGenerateRoute,
+      initialRoute: Routes.home,
+      // home: const Scaffold(body: Center(child: Text('Hello World')),),
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:note_plus/app/my_app.dart';
+import 'package:note_plus/core/routing/app_router.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp( MyApp(appRouter: AppRouter()));
 }

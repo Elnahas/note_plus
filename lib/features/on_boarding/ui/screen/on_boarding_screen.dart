7021bdf03cfafb7d9eef421c2a9cef0011b3a8da
Hazem <hazemelnahas2010@gmail.com>
2024-08-12 14:39:17 +0300

    - refactor and split OnBoardingBlocConsumer

--- lib/features/on_boarding/ui/screen/on_boarding_screen.dart
+++ lib/features/on_boarding/ui/screen/on_boarding_screen.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:note_plus/core/helpers/app_string.dart';
-import 'package:note_plus/core/helpers/extentions.dart';
-import 'package:note_plus/core/routing/routes.dart';
 import 'package:note_plus/core/theming/app_colors.dart';
 import '../../../../core/helpers/spacing.dart';
 import '../../../../core/theming/app_text_styles.dart';
-import '../../../../core/widgets/app_elevated_button.dart';
 import '../../logic/on_boarding_cubit.dart';
+import '../widgets/on_boarding_bloc_consumer.dart';
 import '../widgets/page_view_widget.dart';
 
 class OnBoardingScreen extends StatelessWidget {
@@ -44,63 +41,7 @@ class OnBoardingScreen extends StatelessWidget {
                 },
               ),
               verticalSpace(50),
-              BlocConsumer<OnBoardingCubit, OnBoardingState>(
-                listenWhen: (previous, current) =>
-                    current is OnBoardingCompleted,
-                listener: (context, state) {
-                  context.pushReplacementNamed(Routes.home);
-                },
-                buildWhen: (previous, current) =>
-                    current is OnBoardingPageChanged,
-                builder: (context, state) {
-                  int currentPage = 0;
-                  if (state is OnBoardingPageChanged) {
-                    currentPage = state.currentPage;
-                  }
-
-                  return Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      if (currentPage > 0)
-                        GestureDetector(
-                          onTap: () {
-                            context
-                                .read<OnBoardingCubit>()
-                                .onBackPage(currentPage);
-                            controller.previousPage(
-                                duration: const Duration(milliseconds: 200),
-                                curve: Curves.easeInOut);
-                          },
-                          child: Text(
-                            AppString.back,
-                            style: AppTextStyle.font16WhiteRegularOpacity44,
-                          ),
-                        ),
-                      if (currentPage == 0) const SizedBox(),
-                      AppElevatedButton(
-                        height: 48.h,
-                        width: 90.w,
-                        borderRadius: 4.r,
-                        buttonText: currentPage == 2
-                            ? AppString.getStarted
-                            : AppString.next,
-                        onPressed: () {
-                          if (currentPage < 2) {
-                            context
-                                .read<OnBoardingCubit>()
-                                .onNextPage(currentPage);
-                            controller.nextPage(
-                                duration: const Duration(milliseconds: 500),
-                                curve: Curves.easeInOut);
-                          } else {
-                            context.read<OnBoardingCubit>().onSkip();
-                          }
-                        },
-                      ),
-                    ],
-                  );
-                },
-              )
+              OnBoardingBlocConsumer(controller: controller)
             ],
           ),
         ),

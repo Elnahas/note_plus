c36d927be43766a2a25f6269a277a7bdd975333a
Hazem <hazemelnahas2010@gmail.com>
2024-08-19 07:36:19 +0300

    - add time line DatePicker

--- lib/features/home/ui/screen/home_screen.dart
+++ lib/features/home/ui/screen/home_screen.dart
@@ -1,5 +1,9 @@
+import 'package:date_picker_timeline/date_picker_timeline.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:intl/intl.dart';
+import 'package:note_plus/core/helpers/app_string.dart';
+import 'package:note_plus/core/helpers/spacing.dart';
 import '../../../../core/theming/app_colors.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -16,11 +20,34 @@ class HomeScreen extends StatelessWidget {
         child: Container(
           margin: const EdgeInsets.all(24),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Text(
                 DateFormat.yMMMMd().format(DateTime.now()),
                 style: Theme.of(context).textTheme.displayLarge,
-              )
+              ),
+              verticalSpace(10),
+              Text(
+                AppString.today,
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              verticalSpace(10),
+              DatePicker(
+                DateTime.now(),
+                height: 90.h,
+                initialSelectedDate: DateTime.now(),
+                selectionColor: AppColors.primary,
+                selectedTextColor: Colors.white,
+                dayTextStyle: const TextStyle(color: Colors.white),
+                dateTextStyle: const TextStyle(color: Colors.white),
+                monthTextStyle: const TextStyle(color: Colors.white),
+                onDateChange: (date) {
+                  // New date selected
+                  // setState(() {
+                  //   _selectedValue = date;
+                  // });
+                },
+              ),
             ],
           ),
         ),

91a39ba0f6df1c5f4496e6fb1dd1325156cc3243
Hazem <hazemelnahas2010@gmail.com>
2024-08-19 07:23:00 +0300

    - add format date by intl package

--- lib/app/my_app.dart
+++ lib/app/my_app.dart
@@ -3,7 +3,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:note_plus/core/helpers/app_string.dart';
 import 'package:note_plus/core/routing/app_router.dart';
 import '../core/helpers/constants.dart';
-import '../core/helpers/shared_pref_helper.dart';
 import '../core/routing/routes.dart';
 import '../core/theming/theme.dart';
 
--- lib/features/home/ui/screen/home_screen.dart
+++ lib/features/home/ui/screen/home_screen.dart
@@ -1,12 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import '../../../../core/theming/app_colors.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: Text('Home Screen')),
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+          backgroundColor: AppColors.primary,
+          child: const Icon(Icons.add),
+          onPressed: () {}),
+      body: SafeArea(
+        child: Container(
+          margin: const EdgeInsets.all(24),
+          child: Column(
+            children: [
+              Text(
+                DateFormat.yMMMMd().format(DateTime.now()),
+                style: Theme.of(context).textTheme.displayLarge,
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
